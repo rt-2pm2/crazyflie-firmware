2c7040880e8cab0c22620f34b2570800d7847b7e
Luigi Pannocchi <l.pannocchi@gmail.com>
2019-06-18 18:08:27 -0700

    Completed the embedding of the estimator_kalmanUSC

--- src/modules/src/estimator.c
+++ src/modules/src/estimator.c
@@ -5,6 +5,7 @@
 #include "estimator.h"
 #include "estimator_complementary.h"
 #include "estimator_kalman.h"
+#include "estimator_kalmanUSC.h" 
 
 #define DEFAULT_ESTIMATOR complementaryEstimator
 static StateEstimatorType currentEstimator = anyEstimator;
@@ -23,6 +24,7 @@ typedef struct {
   bool (*estimatorEnqueueTOF)(const tofMeasurement_t *tof);
   bool (*estimatorEnqueueAbsoluteHeight)(const heightMeasurement_t *height);
   bool (*estimatorEnqueueFlow)(const flowMeasurement_t *flow);
+  bool (*estimatorEnqueuePose)(const poseMeasurement_t *pose);
 } EstimatorFcns;
 
 #define NOT_IMPLEMENTED ((void*)0)
@@ -75,12 +77,7 @@ static EstimatorFcns estimatorFunctions[] = {
     .test = estimatorKalmanUSCTest,
     .update = estimatorKalmanUSC,
     .name = "KalmanUSC",
-    .estimatorEnqueueTDOA = estimatorKalmanUSCEnqueueTDOA,
     .estimatorEnqueuePosition = estimatorKalmanUSCEnqueuePosition,
-    .estimatorEnqueueDistance = estimatorKalmanUSCEnqueueDistance,
-    .estimatorEnqueueTOF = estimatorKalmanUSCEnqueueTOF,
-    .estimatorEnqueueAbsoluteHeight = estimatorKalmanUSCEnqueueAbsoluteHeight,
-    .estimatorEnqueueFlow = estimatorKalmanUSCEnqueueFlow,
 	.estimatorEnqueuePose = estimatorKalmanUSCEnqueuePose,
     },
 };
